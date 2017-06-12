5c13b27d95a78617c11783786ee8639bc7554b8d
osamamohsen <osamamohsen1994@gmail.com>
2017-06-13 01:28:43 +0200

    add Relative Layout

--- Tutrial003/app/src/main/java/com/example/osama/tutrial003/MainActivity.java
+++ Tutrial003/app/src/main/java/com/example/osama/tutrial003/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.osama.tutrial003;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * add an new Second Activity
+     * @param v
+     */
+    public void goToSecondActivity(View v){
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
